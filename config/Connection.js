e989f162643f9f896086e92b7615f3643ce171c1
mathesbh <33523475+mathesbh@users.noreply.github.com>
2020-07-29 07:14:16 -0300

    delete local banco

--- config/Connection.js
+++ config/Connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const connectionString = 'mongodb+srv://user:1234@cluster0-srdis.mongodb.net/trabalhoFinal?retryWrites=true&w=majority'
+const connectionString = 'localbanco'
 
 const openConnection = () => mongoose.connect(connectionString, { useNewUrlParser: true })
 
