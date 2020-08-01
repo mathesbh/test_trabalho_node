2c6f7e866ef833ea4934a0bb2ccb8569b10fe2f6
mathesbh <mathesbh@gmail.com>
2020-08-01 13:02:06 -0300

    local bd

--- config/Connection.js
+++ config/Connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const connectionString = 'localbanco'
+const connectionString = 'mongodb+srv://user:1234@cluster0-srdis.mongodb.net/trabalhoFinal?retryWrites=true&w=majority'
 
 const openConnection = () => mongoose.connect(connectionString, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false })
 
